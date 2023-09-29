798b1799f32a8cb244f07d314ebf184e9d13643a
MahbubulRishad <mahbubulrishad@gmail.com>
2023-09-30 02:57:12 +0600

    Rest assured and Bearer Token

--- src/test/java/com/adequateshop/restapi/test/auth/read/getAllUsers.java
+++ src/test/java/com/adequateshop/restapi/test/auth/read/getAllUsers.java
@@ -5,13 +5,17 @@ import com.adequateshop.restapi.test.auth.bearerToken.BearerToken;
 import com.adequateshop.restapi.test.auth.write.PostLoginTest;
 import com.adequateshop.restapi.utill.General;
 import io.restassured.http.ContentType;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.Matchers;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class getAllUsers extends BaseApiTest {
     @Test
-    public void getAllUsers(){
+    public void getAllUsers() {
         given()
                 .contentType(ContentType.JSON)
                 .header("Authorization", "Bearer " + BearerToken.getBearerToken())
@@ -21,5 +25,31 @@ public class getAllUsers extends BaseApiTest {
                 .then()
                 .statusCode(200)
                 .log().body();
+    }
+
+    @Test
+    public void getDynamicUserByID() {
+        ValidatableResponse validatableResponse = given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + BearerToken.getBearerToken())
+                .log().uri()
+                .when()
+                .get("/users?page=1")
+                .then()
+                .log().body()
+                .statusCode(200);
+
+        int id = validatableResponse.extract().jsonPath().getInt("data[0].id");
+
+        given()
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer " + BearerToken.getBearerToken())
+                .log().uri()
+                .when()
+                .get("/users/"+ id)
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("id", equalTo(id));
     }
 }
